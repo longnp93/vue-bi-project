fb3485473037fc115af441ae1c7db03adbf24d96
Long Nguyen <longnp93@gmail.com>
2021-02-07 22:48:53 +0100

    fallback routes

--- bi-app/src/components/PowerBI.vue
+++ bi-app/src/components/PowerBI.vue
@@ -1,5 +1,6 @@
 <template>
     <div>
         <h1>Power BI</h1>
+        <iframe width="800" height="836" src="https://app.powerbi.com/view?r=eyJrIjoiZWQwY2RkY2ItZTAyMS00ODI4LWJmM2UtYjczYzY1MDc2OWEzIiwidCI6ImQzODY0Y2YyLTUyY2QtNGFiZC05MmY1LTk0ZDMzY2VhM2U0ZCJ9" frameborder="0" allowFullScreen="true"></iframe>
     </div>
 </template>
