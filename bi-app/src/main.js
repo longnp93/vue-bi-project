d748a729ef9c9f182da1a612a93f7b9d631cc3f1
Long Nguyen <longnp93@gmail.com>
2021-02-07 14:34:08 +0100

    jointJS

--- bi-app/src/main.js
+++ bi-app/src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import router from './router'
+import * as joint from 'jointjs'
 Vue.config.productionTip = false
+import 'bootstrap/dist/css/bootstrap.min.css'
+Vue.prototype.$joint=joint;
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
